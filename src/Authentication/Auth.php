2475ba1bdf9faf6d5da3a3d8bf538a584d06cc43
matheusdacs <matheusdacs@gmail.com>
2021-11-08 14:24:45 -0300

    6

--- src/Authentication/Auth.php
+++ src/Authentication/Auth.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ftrmkt/Authentication;
+namespace FuturaMkt\Authentication;
 
 class Auth{
 
--- src/Authentication/Meli/Meli.php
+++ src/Authentication/Meli/Meli.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace FuturaMkt/Authentication/Meli;
+namespace FuturaMkt\Authentication\Meli;
 
 use GuzzleHttp\Client;
 
